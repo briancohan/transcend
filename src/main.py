1d31dc88457d9e3c3cb4738c6e5183d8c41d2c6e
Brian Cohan <briancohan@gmail.com>
2025-02-11 14:56:15 -0500

    mvp

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import io
 
-from flask import Flask, render_template, send_file
+from flask import Flask, redirect, render_template, request, send_file, url_for
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from wordcloud import STOPWORDS, WordCloud
@@ -8,10 +8,9 @@ from wordcloud import STOPWORDS, WordCloud
 from src.db import Labels, engine
 
 app = Flask(__name__)
-session = Session(engine)
 
 
-def generate_wordcloud(text):
+def generate_wordcloud(text: str):
     stopwords = set(STOPWORDS)
 
     wc = WordCloud(
@@ -31,22 +30,45 @@ def generate_wordcloud(text):
 
 @app.route("/sar.png")
 def sar_wc():
-    result = session.execute(select(Labels.sar)).fetchall()
+    with engine.connect() as conn:
+        result = conn.execute(select(Labels.sar)).fetchall()
     return generate_wordcloud(" ".join([row[0] for row in result]))
 
 
 @app.route("/work.png")
 def work_wc():
-    result = session.execute(select(Labels.work)).fetchall()
+    with engine.connect() as conn:
+        result = conn.execute(select(Labels.work)).fetchall()
     return generate_wordcloud(" ".join([row[0] for row in result]))
 
 
 @app.route("/home.png")
 def home_wc():
-    result = session.execute(select(Labels.home)).fetchall()
+    with engine.connect() as conn:
+        result = conn.execute(select(Labels.home)).fetchall()
     return generate_wordcloud(" ".join([row[0] for row in result]))
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    images = [
+        ("When we are in SAR", "sar", "/sar.png"),
+        ("When we are at work", "work", "/work.png"),
+        ("When we are at home", "home", "/home.png"),
+    ]
+    return render_template("index.html", images=images)
+
+
+@app.route("/create", methods=["POST"])
+def add_record():
+    if request.form["sar"] and request.form["work"] and request.form["home"]:
+        with Session(engine) as session:
+            label = Labels(
+                sar=request.form["sar"].lower(),
+                work=request.form["work"].lower(),
+                home=request.form["home"].lower(),
+            )
+            session.add(label)
+            session.commit()
+
+    return redirect(url_for("index"))
